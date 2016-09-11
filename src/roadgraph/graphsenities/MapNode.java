502c9efb72ab812bbf390b955522571372b85963
sanekas <aleksandr.o.sergeev@gmail.com>
2016-09-11 10:22:40 +0300

    Refactored

--- src/roadgraph/graphsenities/MapNode.java
+++ src/roadgraph/graphsenities/MapNode.java
@@ -53,6 +53,7 @@ public class MapNode implements Comparable<MapNode> {
     public String toString() {
         return "MapNode{" +
                 "location=" + location +
+                ", weight=" + weight +
                 '}';
     }
 }
